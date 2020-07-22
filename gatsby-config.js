11c9f8af198a88641840438401b8278ba4588eb4
theshahriyar <shahriyar.31dec@gmail.com>
2020-07-22 10:44:19 +0600

    sitemap path change

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,15 +9,13 @@ module.exports = {
 
   siteMetadata: {
     title: "Shahriyar Ahmed - Front End Developer",
-    description:
-      "I'm Shahriyar Ahmed and a Frontend Web Developer. I'm expert in HTML, CSS3, jQuery, Javascript, Bootstrap Framework, React, Angular Photoshop and many more. Generally, I develop UI of an application and website and also convert PSD to HTML website. All of my website is 100% responsive and those are perfect in all size of devices and all of those are modern browser supported. My HTML code is W3C validated and SEO friendly.",
+    description: "I'm Shahriyar Ahmed and a Frontend Web Developer. I'm expert in HTML, CSS3, jQuery, Javascript, Bootstrap Framework, React, Angular Photoshop and many more. Generally, I develop UI of an application and website and also convert PSD to HTML website. All of my website is 100% responsive and those are perfect in all size of devices and all of those are modern browser supported. My HTML code is W3C validated and SEO friendly.",
     author: "TheShahriyar",
     image: "/thumbnail.jpg",
     siteUrl: "https://theshahriyar.com",
   },
 
-  plugins: [
-    {
+  plugins: [{
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
@@ -27,9 +25,12 @@ module.exports = {
     {
       resolve: "gatsby-plugin-robots-txt",
       options: {
-        host: "https://theshahriyar.com",
-        sitemap: "https://theshahriyar.com/sitemap.xml",
-        policy: [{ userAgent: "*", allow: "/" }],
+        host: "https://portfolio.theshahriyar.com",
+        sitemap: "https://portfolio.theshahriyar.com/sitemap.xml",
+        policy: [{
+          userAgent: "*",
+          allow: "/"
+        }],
       },
     },
     `gatsby-transformer-sharp`,
