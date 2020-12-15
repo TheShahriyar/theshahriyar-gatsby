f9188cb6b4b585055e753abf98d167f2b5fdca75
theshahriyar <shahriyar.31dec@gmail.com>
2020-12-15 11:54:02 +0600

    gatsby-config file updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     description: "I'm Shahriyar Ahmed and a Frontend Web Developer. I'm expert in HTML, CSS3, jQuery, Javascript, Bootstrap Framework, React, Angular Photoshop and many more. Generally, I develop UI of an application and website and also convert PSD to HTML website. All of my website is 100% responsive and those are perfect in all size of devices and all of those are modern browser supported. My HTML code is W3C validated and SEO friendly.",
     author: "TheShahriyar",
     image: "/thumbnail.jpg",
-    siteUrl: "https://theshahriyar.com",
+    siteUrl: "https://portfolio.theshahriyar.com",
   },
 
   plugins: [{
